d6f7174d77d851e5f29beecb855fb0606638eb6e
artacone <artacone@gmail.com>
2022-04-02 19:23:09 +0300

    Ad arg processing for client

--- client/include/main.h
+++ client/include/main.h
@@ -10,14 +10,18 @@
 #include <unistd.h>
 #include <fcntl.h>
 
-#define	SERVER_PORT	50000
-#define	SERVER_IP	"127.0.0.1"
+#define USAGE			"Usage:\n./client.out [server_ip server_port file]\n"
+#define	DEFAULT_PORT	50000
+#define	DEFAULT_IP		"127.0.0.1"
 
-#define	BUF_SIZE	256
+#define	BUF_SIZE		4096
 
 typedef struct s_client {
 	int					fd;
 	struct sockaddr_in	address;
+	int					server_port;
+	const char			*server_ip;
+	const char			*file_name;
 } t_client;
 
 #endif
--- client/src/main.c
+++ client/src/main.c
@@ -1,15 +1,44 @@
 #include "../include/main.h"
 #include "../include/wrappers.h"
 
+void check_args(int argc, char *argv[], t_client *client) {
+	if (argc == 1) {
+		client->server_port = DEFAULT_PORT;
+		client->server_ip = DEFAULT_IP;
+		client->file_name = "send.txt"
+	} else if (argc == 4) {
+		client->server_ip = agrv[1];
+		int port = atoi(argv[2]);
+		if (0 < port && port < 65536) {
+			client->server_port = port;
+		} else {
+			fprintf(stderr, "Wrong port number!\n");
+			fprintf(stderr, USAGE);
+			exit(EXIT_FAILURE);
+		}
+		if (0 == access(argv[3], R_OK)) {
+			client->file_name = argv[3];
+		} else {
+			perror("access failed");
+			fprintf(stderr, USAGE);
+			exit(EXIT_FAILURE);
+		}
+	} else {
+		fprintf(stderr, USAGE);
+		exit(EXIT_FAILURE);
+	}
+}
+
 void init_client(t_client *client) {
 	client->fd = Socket(AF_INET, SOCK_STREAM, 0);
 	client->address.sin_family = AF_INET;
-	client->address.sin_port = htons(SERVER_PORT);
-	Inet_pton(AF_INET, SERVER_IP, &client->address.sin_addr);
+	client->address.sin_port = htons(client->server_port);
+	Inet_pton(AF_INET, client->server_ip, &client->address.sin_addr);
 }
 
 // TODO proper loop around write
-void send_file(int file_fd, t_client *client) { // or just use sendfile()
+void send_file(t_client *client) { // or just use sendfile()
+	int file_fd = open(client->file_name, O_RDONLY); // protect
 	char buf[BUF_SIZE] = {0};
 	int n_read = 0;
 	while ((n_read = read(file_fd, buf, sizeof buf))) { // FIXME errors
@@ -19,20 +48,18 @@ void send_file(int file_fd, t_client *client) { // or just use sendfile()
 		}
 		write(client->fd, buf, n_read); // protect
 	}
+	close(file_fd);
 }
 
-int main() {
+int main(int argc, char *argv[]) {
 	t_client client = {0};
+	check_args(argc, argv, &client);
 	init_client(&client);
 
 	Connect(client.fd, (struct sockaddr *) &client.address, sizeof client.address);
 
-	char *file_name = "send.txt";
-	int file_fd = open(file_name, O_RDONLY); // protect
+	send_file(&client);
 
-	send_file(file_fd, &client);
-
-	close(file_fd);
 	close(client.fd);
 	return (0);
 }
